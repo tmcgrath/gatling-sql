297f405c80180bbf1a3cb0583240a01be96aada2
Todd McGrath <mcgrath@supergloo.com>
2017-09-11 14:55:04 -0500

    remove unneeded comment

--- src/test/scala/io/github/gatling/sql/example/ThriftServerSimulation.scala
+++ src/test/scala/io/github/gatling/sql/example/ThriftServerSimulation.scala
@@ -4,11 +4,7 @@ import io.gatling.core.Predef._
 import io.github.gatling.sql.Predef._
 import io.github.gatling.sql.db.ConnectionPool
 
-/**
-  *
-  * uncomment the hive-jdbc and hadoop-common dependecies in pom.xml to run this example
-  *
-  */
+
 class ThriftServerSimulation extends Simulation {
 
   Class.forName("org.apache.hive.jdbc.HiveDriver")
@@ -16,10 +12,13 @@ class ThriftServerSimulation extends Simulation {
   val conn = ConnectionPool.connection
   val sqlConfig = sql.connection(conn)
 
-  // Assumes data is already loaded into a Cassandra keyspace named 'pioneer'
-  // and tables `students` or `teachers` populated with data
+  // Assumes data is already loaded into a Cassandra keyspace named 'killrweather'
 
-  val sqlQuery = "SELECT NAME FROM pioneer.${table} WHERE ID = ${id}"
+  val joinQuery = """SELECT ws.name, raw.temperature
+                  FROM raw_weather_data raw JOIN weather_station ws
+                  ON raw.wsid=ws.id WHERE raw.wsid = '725030:14732'
+                  AND raw.year = 2008 AND raw.month = 12 AND raw.day = 31"""
+
   val sqlAllQuery = "SELECT * FROM pioneer.${table} WHERE ID = ${id}"
 
   def scn =
